e075b1c39482e1240ef18e4206f6509e0a69e225
Yoshikazu Miyoshi <y.miyoshi@gloops.com>
2012-12-26 18:47:18 +0900

    メモリリークを修正した

--- NSEnumerator+Query.m
+++ NSEnumerator+Query.m
@@ -13,7 +13,7 @@
     self = [super init];
     if(self) {
         _src = src;
-        _nextObject = [nextObject copy];
+        _nextObject = [[nextObject copy]autorelease];
     }
     return self;
 }
@@ -33,13 +33,13 @@
     __weak NSData *weakSelf = self;
     __block int counter = 0;
     
-    return [[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
+    return [[[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
         while (counter < [weakSelf length]) {
             
             return [NSNumber numberWithChar:(*((char *)([weakSelf bytes] + counter++)))];
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 @end
@@ -50,12 +50,12 @@
     __weak NSString *weakSelf = self;
     __block int counter = 0;
     
-    return [[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
+    return [[[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
         while (counter < [weakSelf length]) {
             return [NSNumber numberWithUnsignedShort:[weakSelf characterAtIndex:counter++]];
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 @end
@@ -64,72 +64,72 @@
 
 +(NSEnumerator *)range:(int)start to:(int)count {
     __block int counter = start;
-    return [[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
+    return [[[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
         while (counter < (start + count)) {
             return [NSNumber numberWithInt:counter++];
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 
 +(NSEnumerator *)repeat:(id)item count:(int)count {
     __block int counter = 0;
-    return [[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
+    return [[[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
         while (counter < count) {
             counter++;
             return item;
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 +(NSEnumerator *)empty {
-    return [[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
+    return [[[CustomEnumerator alloc]initWithFunction:nil nextObjectBlock:^id(NSEnumerator * src) {
         return nil;
-    }];
+    }] autorelease];
 }
 
 - (NSEnumerator *) ofClass: (Class) classType
 {
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         do {
             item = [src nextObject];
         } while (item != nil && ![item isKindOfClass:classType]);
 
         return item;
-    }];
+    }] autorelease];
 }
 
 
 -(NSEnumerator *) select: (id(^)(id)) selector
 {
-    id (^_selector)(id) = [selector copy];
+    id (^_selector)(id) = [[selector copy]autorelease];
     return [self selectWithIndex:^id(id item, int index) {
         return _selector(item);
-    }];
+    }] ;
 }
 
 
 -(NSEnumerator *) selectWithIndex: (id(^)(id,int)) selector
 {
     __block int counter = 0;
-    id (^_selector)(id,int) = [selector copy];
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    id (^_selector)(id,int) = [[selector copy]autorelease];
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while ((item = [src nextObject]))
         {
             return _selector(item,counter++);
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 
 -(NSEnumerator *) where: (BOOL(^)(id)) predicate
 {
-    BOOL (^_predicate)(id) = [predicate copy];
+    BOOL (^_predicate)(id) = [[predicate copy]autorelease];
     return [self whereWithIndex:^BOOL(id item, int index) {
         return _predicate(item);
     }];
@@ -139,8 +139,8 @@
 -(NSEnumerator *) whereWithIndex: (BOOL(^)(id,int)) predicate
 {
     __block int counter = 0;
-    BOOL (^_predicate)(id,int) = [predicate copy];
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    BOOL (^_predicate)(id,int) = [[predicate copy]autorelease];
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while ((item = [src nextObject]))
         {
@@ -148,14 +148,14 @@
             return item;
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 
 -(NSEnumerator *) skip: (int)count
 {
     __block int counter = 0;
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while (counter++ < count)
         {
@@ -163,7 +163,7 @@
                 return nil;
         }
         return [src nextObject];
-    }];
+    }] autorelease];
 }
 
 
@@ -179,8 +179,8 @@
 {
     __block int counter = 0;
     __block BOOL skipped = NO;
-    BOOL (^_predicate)(id,int) = [predicate copy];
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    BOOL (^_predicate)(id,int) = [[predicate copy]autorelease];
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         if (!skipped)
         {
@@ -192,21 +192,21 @@
             return item;
         }
         return [src nextObject];
-    }];
+    }] autorelease];
 }
 
 
 -(NSEnumerator *) take: (int)count
 {
     __block int counter = 0;
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         if (counter++ < count && (item = [src nextObject]))
         {
             return item;
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 
@@ -222,7 +222,7 @@
 {
     __block int counter = 0;
     __block BOOL taking = YES;
-    BOOL (^_predicate)(id,int) = [predicate copy];
+    BOOL (^_predicate)(id,int) = [[predicate copy]autorelease];
     return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         if (taking && (item = [src nextObject]))
@@ -237,10 +237,10 @@
 }
 
 -(NSEnumerator *) scan: (id(^)(id,id)) func {
-    id (^_func)(id,id) = [func copy];
+    id (^_func)(id,id) = [[func copy]autorelease];
     __block BOOL first = YES;
     __block id result;
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while ((item = [src nextObject]))
         {
@@ -253,14 +253,14 @@
             return result;
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 -(NSEnumerator *)orderByDescription:(NSSortDescriptor *)firstObj, ... NS_REQUIRES_NIL_TERMINATION
 {
     va_list list;
     va_start(list, firstObj);
-    NSMutableArray *array = [[NSMutableArray alloc]initWithObjects:firstObj, nil];
+    NSMutableArray *array = [[[NSMutableArray alloc]initWithObjects:firstObj, nil] autorelease];
     NSSortDescriptor *desc;
     while((desc = va_arg(list, NSSortDescriptor*)))
     {
@@ -274,9 +274,9 @@
 
 - (NSEnumerator *) selectMany: (id(^)(id)) selector
 {
-    id (^_selector)(id) = [selector copy];
+    id (^_selector)(id) = [[selector copy]autorelease];
     __block id current = [self nextObject];
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while ((item = [current nextObject]))
         {
@@ -287,13 +287,13 @@
             return [current nextObject];
         }
         return nil;
-    }];
+    }] autorelease];
 }
 
 - (NSEnumerator *) distinct{
     
     __block NSMutableArray *returnedArray = [[NSMutableArray alloc]init];
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         while((item = [src nextObject]) != nil && [returnedArray containsObject:item]){
             NSLog(@"skip : %@",item);
@@ -305,14 +305,14 @@
             return item;
         }
         return item;
-    }];
+    }] autorelease];
 }
 
 - (NSEnumerator *) concat:(NSEnumerator *)dst
 {
     NSEnumerator *_dst = dst;
     __block BOOL isFirst = true;
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         id item;
         if (isFirst)
         {
@@ -330,7 +330,7 @@
         {
             return [_dst nextObject];
         }
-    }];
+    }] autorelease];
 }
 
 
@@ -354,10 +354,10 @@
 
 - (NSEnumerator *) buffer:(int)count;
 {
-    return [[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
+    return [[[CustomEnumerator alloc]initWithFunction:self nextObjectBlock:^id(NSEnumerator *src) {
         NSArray *result = [[src take:count]toArray];
         return (result.count == 0) ? nil: result;
-    }];
+    }] autorelease];
 }
 
 - (NSArray*) toArray
@@ -367,7 +367,7 @@
 
 - (NSMutableArray*) toMutableArray
 {
-    NSMutableArray *result = [[NSMutableArray alloc]init];
+    NSMutableArray *result = [[[NSMutableArray alloc]init] autorelease];
     for (id value in self) {
         [result addObject:value];
     }
@@ -380,7 +380,7 @@
     NSArray* keyArray = [[[objArray objectEnumerator] select:^id(id item) {
         return keySelector(item);
     }]toArray];
-    return [[NSDictionary alloc]initWithObjects:objArray forKeys:keyArray];
+    return [[[NSDictionary alloc]initWithObjects:objArray forKeys:keyArray] autorelease];
 }
 
 - (NSDictionary *) toDictionary: (id(^)(id)) keySelector elementSelector:(id(^)(id)) elementSelector{
@@ -391,7 +391,7 @@
     NSArray* elementArray = [[[objArray objectEnumerator] select:^id(id item) {
         return elementSelector(item);
     }]toArray];
-    return [[NSDictionary alloc]initWithObjects:elementArray forKeys:keyArray];
+    return [[[NSDictionary alloc]initWithObjects:elementArray forKeys:keyArray] autorelease];
 }
 
 - (NSMutableDictionary *) toMutableDictionary: (id(^)(id)) keySelector{
@@ -400,7 +400,7 @@
     NSArray* keyArray = [[[objArray objectEnumerator] select:^id(id item) {
         return keySelector(item);
     }]toArray];
-    return [[NSMutableDictionary alloc]initWithObjects:objArray forKeys:keyArray];
+    return [[[NSMutableDictionary alloc]initWithObjects:objArray forKeys:keyArray] autorelease];
 }
 
 - (NSMutableDictionary *) toMutableDictionary: (id(^)(id)) keySelector elementSelector:(id(^)(id)) elementSelector{
@@ -411,12 +411,12 @@
     NSArray* elementArray = [[[objArray objectEnumerator] select:^id(id item) {
         return elementSelector(item);
     }]toArray];
-    return [[NSMutableDictionary alloc]initWithObjects:elementArray forKeys:keyArray];
+    return [[[NSMutableDictionary alloc]initWithObjects:elementArray forKeys:keyArray] autorelease];
 }
 
 -(NSData *) toData {
     NSArray * array = [self allObjects];
-    NSMutableData *result = [[NSMutableData alloc]initWithCapacity:[array count]];
+    NSMutableData *result = [[[NSMutableData alloc]initWithCapacity:[array count]] autorelease];
     for (NSNumber * obj in array) {
         char charByte = [obj charValue];
         [result appendBytes:&charByte length:1];
@@ -425,7 +425,7 @@
 }
 
 -(NSString *) toString {
-    __block NSString *str = [[NSString alloc]init];
+    __block NSString *str = [[[NSString alloc]init] autorelease];
     [self forEach:^(id number) {
         unichar charShort = [(NSNumber*)number unsignedShortValue];
         [str stringByAppendingString:[NSString stringWithCharacters:&charShort length:1]];
